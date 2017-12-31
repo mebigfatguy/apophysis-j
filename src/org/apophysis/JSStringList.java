6325e4adf152af6e8303a246fb3eaa991da0f028
Dave Brosius <dbrosius@mebigfatguy.com>
2017-12-31 13:26:28 -0500

    remove dead allocations

--- src/org/apophysis/JSStringList.java
+++ src/org/apophysis/JSStringList.java
@@ -11,72 +11,50 @@ import java.util.List;
 
 public class JSStringList implements Constants {
 
-	/*****************************************************************************/
-	// FIELDS
+    /*****************************************************************************/
+    // FIELDS
 
-	public int Count = 0;
-	public String[] Strings = new String[0];
+    private List<String> lines = new ArrayList<>();
 
-	private List<String> lines = new ArrayList<String>();
+    /*****************************************************************************/
 
-	/*****************************************************************************/
+    public JSStringList() {
+    }
 
-	public JSStringList() {
-	}
+    /*****************************************************************************/
 
-	/*****************************************************************************/
+    public void add(String line) {
+        lines.add(line);
+    }
 
-	public JSStringList Create() {
-		return new JSStringList();
-	}
+    /*****************************************************************************/
 
-	/*****************************************************************************/
+    public void saveToFile(String filename) {
+        try (PrintWriter w = new PrintWriter(new BufferedWriter(new FileWriter(filename)))) {
+            for (String line : lines) {
+                w.println(line);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
-	public void Add(String line) {
-		lines.add(line);
-		updateFields();
-	}
+    /*****************************************************************************/
 
-	/*****************************************************************************/
+    public void loadFromFile(String filename) throws IOException {
+        lines = new ArrayList<>();
 
-	private void updateFields() {
-		Count = lines.size();
-		Strings = new String[Count];
-		for (int i = 0; i < Count; i++) {
-			Strings[i] = lines.get(i);
-		}
-	}
+        try (BufferedReader r = new BufferedReader(new FileReader(filename))) {
+            while (true) {
+                String line = r.readLine();
+                if (line == null) {
+                    break;
+                }
+                lines.add(line);
+            }
+        }
+    }
 
-	/*****************************************************************************/
-
-	public void SaveToFile(String filename) {
-		try (PrintWriter w = new PrintWriter(new BufferedWriter(new FileWriter(filename)))) {
-			for (String line : lines) {
-				w.println(line);
-			}
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
-	}
-
-	/*****************************************************************************/
-
-	public void LoadFromFile(String filename) throws IOException {
-		lines = new ArrayList<String>();
-
-		try (BufferedReader r = new BufferedReader(new FileReader(filename))) {
-    		while (true) {
-    			String line = r.readLine();
-    			if (line == null) {
-    				break;
-    			}
-    			lines.add(line);
-    		}
-		}
-
-		updateFields();
-	}
-
-	/*****************************************************************************/
+    /*****************************************************************************/
 
 } // End of class JSStringList
